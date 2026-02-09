2c65a8fc91f752c9c24f7d40f605ffcd0c39d8bd
Skyltliu <skyltliu@gmail.com>
2026-02-09 14:39:14 -0600

    added bottom-up dp solution

--- Leetcode22 Generate Parentheses/solve.py
+++ Leetcode22 Generate Parentheses/solve.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+    #recursion solution (no caching)
     def generateParenthesis(self, n: int) -> List[str]:
         if n == 0:
             return [""]
@@ -12,3 +13,19 @@ class Solution:
                 for right_string in self.generateParenthesis(n-i-1):
                     answer.append('(' + left_string + ')' + right_string)
         return answer
+    
+    #true bottom up dp solution:
+    #instead of recursing on all possible permutations, we store all possibilities in each dp[i]
+    def generateParenthesis2(self, n: int) -> list[str]:
+        dp = [[] for _ in range(n + 1)]
+        dp[0] = [""]
+
+        for k in range(1, n + 1):
+            ans = []
+            for i in range(k):
+                for left in dp[i]:
+                    for right in dp[k - i - 1]:
+                        ans.append("(" + left + ")" + right)
+            dp[k] = ans
+
+        return dp[n]

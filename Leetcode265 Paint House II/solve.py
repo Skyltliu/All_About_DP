620554a07c4a1328b0cc1129ed7ee1d3a5f1c144
Skyltliu <skyltliu@gmail.com>
2026-02-24 15:53:07 -0600

    added time optimized dp solution

--- Leetcode265 Paint House II/solve.py
+++ Leetcode265 Paint House II/solve.py
@@ -17,3 +17,23 @@ class Solution:
         return min(costs[-1])
     
 #time optimized dp:
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+        if len(costs) == 0:
+            return 0
+        for house in range(1, len(costs)):
+            min_color = second_color = None
+            
+            for color in range(len(costs[0])):
+                if min_color is None or costs[house-1][color] < costs[house-1][min_color]:
+                    second_color = min_color
+                    min_color = color
+                elif second_color is None or costs[house-1][color] < costs[house - 1][second_color]:
+                    second_color = color
+            for k in range(len(costs[0])):
+                if k == min_color:
+                    costs[house][k] += costs[house-1][second_color]
+                else:
+                    costs[house][k] += costs[house-1][min_color]
+            
+        return min(costs[-1])

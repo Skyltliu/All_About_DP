acfabdd68c2db3c8c3b0d5926bf58e5edb5b2924
Skyltliu <skyltliu@gmail.com>
2026-03-01 20:20:41 -0600

    house robber two pointer sol

---
+++ Leetcode 198 House Robber/solve.py
@@ -0,0 +1,24 @@
+from typing import List
+
+#regular 1d dp
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        return dp[-1]
+    
+#two pointer dp
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev1 = 0
+        prev2 = 0
+        for num in nums:
+            curr = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = curr
+        return curr
--- Leetcode 656 Coin Path/solve.py
+++ Leetcode 656 Coin Path/solve.py
@@ -1,6 +1,6 @@
 from typing import List
 
-
+#memoization
 class Solution:
     def cheapestJump(self, coins: List[int], maxJump: int) -> List[int]:
         nxt = [None] * len(coins)
@@ -27,3 +27,34 @@ class Solution:
         while ans[-1] != len(coins) - 1:
             ans.append(nxt[ans[-1]])
         return [x + 1 for x in ans]
+    
+
+#tabulation
+class Solution:
+    def cheapestJump(self, coins: List[int], maxJump: int) -> List[int]:
+        N = len(coins)
+        dp = [float('inf')] * N
+        nxt = [None] * N
+        dp[-1] = 0
+        for i in range(N-2, -1, -1):
+            for j in range(1, maxJump+1):
+                if i + j >= N:
+                    break
+                if coins[i + j] == -1:
+                    continue
+                if dp[i + j] == float('inf'):
+                    continue
+                store = dp[i+j] + coins[i+j]
+                if store < dp[i]:
+                    dp[i] = store
+                    nxt[i] = i + j
+        if dp[0] == float('inf'):
+            return []
+        ans = []
+        curr = 0
+        while curr is not None:
+            ans.append(curr)
+            if curr == N-1:
+                break
+            curr = nxt[curr]
+        return [x+1 for x in ans]

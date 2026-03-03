51bd7b9f3b6d3adcabd73bbd68cf2d765cf2c250
Skyltliu <skyltliu@gmail.com>
2026-03-03 15:00:13 -0600

    optimized pizza with 3n slices solution

--- Leetcode 1388 Pizza With 3n Slices/solve.py
+++ Leetcode 1388 Pizza With 3n Slices/solve.py
@@ -3,20 +3,17 @@ from typing import List
 
 class Solution:
     def maxSizeSlices(self, slices: List[int]) -> int:
-        ms = len(slices)
-        k = ms // 3
-        
         def slicese(arr):
+            k = len(slices) // 3
+            dp = [[0] * (k+1) for _ in range(len(arr) + 1)]
             m = len(arr)
-            dp = [[0] * (k+1) for _ in range(m+1)]
             for i in range(1, m+1):
                 for j in range(1, k+1):
                     dp[i][j] = dp[i-1][j]
-                    take = arr[i-1]
                     if i >= 2:
-                        take += dp[i-2][j-1]
-                    elif j > 1:
-                        continue
-                    dp[i][j] = max(dp[i][j], take)
+                        dp[i][j] = max(dp[i][j], dp[i-2][j-1] + arr[i-1])
+                    elif j == 1:
+                        dp[i][j] = max(dp[i][j], arr[i-1])
+            
             return dp[m][k]
         return max(slicese(slices[:-1]), slicese(slices[1:]))

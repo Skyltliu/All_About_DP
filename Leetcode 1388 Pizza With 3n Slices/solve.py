fe634031928307f098f3ad71581adb2e5cb1b379
Skyltliu <skyltliu@gmail.com>
2026-03-04 17:36:52 -0600

    leetcode 1388 pizza with 3n slices code optimized

--- Leetcode 1388 Pizza With 3n Slices/solve.py
+++ Leetcode 1388 Pizza With 3n Slices/solve.py
@@ -9,11 +9,11 @@ class Solution:
             m = len(arr)
             for i in range(1, m+1):
                 for j in range(1, k+1):
-                    dp[i][j] = dp[i-1][j]
                     if i >= 2:
-                        dp[i][j] = max(dp[i][j], dp[i-2][j-1] + arr[i-1])
+                        dp[i][j] = max(dp[i-1][j], dp[i-2][j-1] + arr[i-1])
                     elif j == 1:
-                        dp[i][j] = max(dp[i][j], arr[i-1])
+                        dp[i][j] = max(dp[i-1][j], arr[i-1])
             
             return dp[m][k]
         return max(slicese(slices[:-1]), slicese(slices[1:]))
+        

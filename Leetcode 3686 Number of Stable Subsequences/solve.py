97c894af7ab2179e3eefacf0bd5a5db483d8adf6
Skyltliu <skyltliu@gmail.com>
2026-03-15 23:51:52 -0500

    leetcode 3686 done

---
+++ Leetcode 3686 Number of Stable Subsequences/solve.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def countStableSubsequences(self, nums: List[int]) -> int:
+        e1 = e2 = o1 = o2 = 0
+        mod = 10**9+7
+        for x in nums:
+            if x % 2 == 0:
+                new_e1 = (1+o1+o2) % mod
+                new_e2 = e1 % mod
+
+                e1 = e1 + new_e1
+                e2 = e2 + new_e2
+
+            else:
+                new_o1 = (1+e1+e2) % mod
+                new_o2 = o1 % mod
+
+                o1 = o1 + new_o1
+                o2 = o2 + new_o2
+        
+        return (e1+e2+o1+o2) % mod

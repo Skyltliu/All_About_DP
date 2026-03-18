5c56544674317ff812c1177c78811c20967a1bba
Skyltliu <skyltliu@gmail.com>
2026-03-18 18:25:27 -0500

    increase readability of house robber solution

--- Leetcode 198 House Robber/solve.py
+++ Leetcode 198 House Robber/solve.py
@@ -9,7 +9,7 @@ class Solution:
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
         for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         return dp[-1]
     
 #two pointer dp

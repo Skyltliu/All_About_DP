daab669ab6c0160b44d95295025fc4ce110aaf5a
Skyltliu <skyltliu@gmail.com>
2026-03-20 18:06:29 -0500

    leetcode 139 word break done

---
+++ Leetcode 139 Word Break/solve.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        words = set(wordDict)
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in words:
+                    dp[i] = True
+        return dp[-1]

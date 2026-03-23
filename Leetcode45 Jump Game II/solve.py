dfffc76fc2adb74e4390b0537648c04b01745134
Skyltliu <skyltliu@gmail.com>
2026-03-22 19:44:03 -0500

    math solution for leetcode 233 number of digit 1s done

--- Leetcode45 Jump Game II/solve.py
+++ Leetcode45 Jump Game II/solve.py
@@ -8,3 +8,20 @@ class Solution:
         for i in range(len(nums) - 2, -1, -1):
             dp[i] = min(dp[i : i + nums[i] + 1]) + 1
         return dp[0]
+    
+
+#greedy:
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        ans = 0
+        l = r = 0
+        n = len(nums)
+        while r < n-1:
+            farthest = r
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+           
+            l = r
+            r = farthest
+            ans += 1
+        return ans

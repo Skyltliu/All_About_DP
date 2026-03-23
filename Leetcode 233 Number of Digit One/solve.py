bbcb646ced69c2109916e4b8a67c7cdaaa28cfb3
Skyltliu <skyltliu@gmail.com>
2026-03-22 23:49:38 -0500

    added digit dp solution and concept

--- Leetcode 233 Number of Digit One/solve.py
+++ Leetcode 233 Number of Digit One/solve.py
@@ -1,4 +1,7 @@
 #pure math:
+from functools import cache
+
+
 class Solution:
     def countDigitOne(self, n: int) -> int:
         countr = 0
@@ -8,3 +11,25 @@ class Solution:
             countr += (n // divider) * i + min(max(n % divider - i + 1, 0), i)
             i *= 10
         return countr
+    
+
+#digit dp
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        h = str(n)
+        @cache
+        def dp(pos, tight, cnt):
+            if len(h) == pos:
+                return cnt
+            ans = temp = 0
+            limit = 9
+            if tight:
+                limit = int(h[pos])
+            for d in range(limit+1):
+                new_tight = tight and d == int(h[pos])
+                if d == 1:
+                    temp = 1
+                ans += dp(pos+1, new_tight, cnt+temp)
+                temp = 0
+            return ans
+        return dp(0, True, 0)
